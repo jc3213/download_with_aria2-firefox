47b2ae875a60562b8a21b028f75e026ff552455f
jc3213 <jc3213@users.noreply.github.com>
2021-01-23 14:32:06 +0800

    math.floor for filesize

--- components/common.js
+++ components/common.js
@@ -135,16 +135,16 @@ function bytesToFileSize(bytes) {
         return bytes + ' B';
     }
     else if (bytes >= 1024 && bytes < 1048576) {
-        return (bytes / 10.24 + 1 | 0) / 100 + ' KB';
+        return (bytes / 10.24 | 0) / 100 + ' KB';
     }
     else if (bytes >= 1048576 && bytes < 1073741824) {
-        return (bytes / 10485.76 + 1 | 0) / 100 + ' MB';
+        return (bytes / 10485.76 | 0) / 100 + ' MB';
     }
     else if (bytes >= 1073741824 && bytes < 1099511627776) {
-        return (bytes / 10737418.24 + 1 | 0) / 100 + ' GB';
+        return (bytes / 10737418.24 | 0) / 100 + ' GB';
     }
     else if (bytes >= 1099511627776) {
-        return (bytes / 10995116277.76 + 1 | 0) / 100 + ' TB';
+        return (bytes / 10995116277.76 | 0) / 100 + ' TB';
     }
 }
 
