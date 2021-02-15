e9210fdc4c1c7972c4d18687348bf39f15c37251
jc3213 <jc3213@users.noreply.github.com>
2021-02-15 22:36:52 +0800

    support json array with {url, filename, folder}

--- modules/newTask/js.js
+++ modules/newTask/js.js
@@ -7,6 +7,29 @@ document.getElementById('setProxy').addEventListener('click', (event) => {
 document.getElementById('submit_btn').addEventListener('click', (event) => {
     var referer = document.getElementById('taskReferer').value;
     var proxy = document.getElementById('setProxy').checked ? document.getElementById('taskProxy').value : '';
-    var url = document.getElementById('taskBatch').value.split('\n').forEach(url => { if (url !== '') downWithAria2({url, referer}, {'all-proxy': proxy}); });
+    var batch = document.getElementById('taskBatch').value;
+    try {
+        JSON.parse(batch).forEach(task => downloadNewTask(task));
+    }
+    catch(error) {
+        batch.split('\n').forEach(url => downloadNewTask({url}));
+    }
     parent.window.postMessage({id: 'newTaskWindow', delay: 1000});
+
+    function downloadNewTask(task, options = {}) {
+        if (!task.url) {
+            return;
+        }
+        var url = task.url;
+        if (proxy) {
+            options['all-proxy'] = proxy;
+        }
+        if (task.filename) {
+            options['out'] = task.filename;
+        }
+        if (task.folder) {
+            options['dir'] = task.folder;
+        }
+        downWithAria2({url, referer}, options);
+    }
 });
