0490ef907246d6900de69e1055876abb5cd777cc
jc3213 <jc3213@users.noreply.github.com>
2021-03-11 15:36:11 +0800

    support multi objects with new line feeds

--- modules/newTask/js.js
+++ modules/newTask/js.js
@@ -5,18 +5,19 @@ document.getElementById('setProxy').addEventListener('click', (event) => {
 document.getElementById('submit_btn').addEventListener('click', (event) => {
     var referer = document.getElementById('taskReferer').value;
     var options = {'all-proxy': document.getElementById('taskProxy').value};
-    var batch = document.getElementById('taskBatch').value;
-    try {
-        batch = JSON.parse(batch);
-        if (Array.isArray(batch)) {
-            batch.forEach(task => downWithAria2(referer ? {...task, referer} : task, options));
+    document.getElementById('taskBatch').value.split('\n').forEach(url => {
+        try {
+            var session = JSON.parse(url);
+            if (Array.isArray(session)) {
+                session.forEach(task => downWithAria2(referer ? {...task, referer} : task, options));
+            }
+            else {
+                downWithAria2(referer ? {...session, referer} : session, options);
+            }
         }
-        else {
-            downWithAria2(referer ? {...batch, referer} : batch, options);
+        catch(error) {
+            downWithAria2({url, referer}, options);
         }
-    }
-    catch(error) {
-        batch.split('\n').forEach(url => downWithAria2({url, referer}, options));
-    }
+    });
     parent.window.postMessage({id: 'newTaskWindow', delay: 1000});
 });
