5aaa50d498ee20e5e1e9036b11ddcfba214ddc49
jc3213 <jc3213@users.noreply.github.com>
2021-04-08 11:30:42 +0800

    update taskMgr for more details

--- modules/taskMgr/js.js
+++ modules/taskMgr/js.js
@@ -6,32 +6,45 @@ addEventListener('message', (event) => {
     gid = event.data.gid;
     url = event.data.url;
     printTaskOption();
-    printTaskDetails();
-    taskManager = setInterval(printTaskDetails, 1000);
+    printTaskManager();
+    taskManager = setInterval(printTaskManager, 1000);
 })
 
-function printTaskDetails() {
+function printTaskManager() {
     jsonRPCRequest(
         {method: 'aria2.tellStatus', gid},
         (result) => {
-            var completed = result.status === 'complete';
+            var completedLength = bytesToFileSize(result.completedLength);
+            var totalLength = bytesToFileSize(result.totalLength);
+            var downloadSpeed = bytesToFileSize(result.downloadSpeed) + '/s';
+            var uploadSpeed = bytesToFileSize(result.uploadSpeed) + '/s';
+            var completeRatio = ((result.completedLength / result.totalLength * 10000 | 0) / 100) + '%';
             var fileName = result.files[0].path ? result.files[0].path.match(/[^\/]+$/)[0] : '';
-            if (result.bittorrent) {
-                var taskName = result.bittorrent.info ? result.bittorrent.info.name : fileName;
-            }
-            else {
-                taskName = fileName || url;
-            }
-            document.getElementById('taskName').innerText = taskName;
-            document.getElementById('taskName').className = 'button title ' + result.status;
+            var completed = result.status === 'complete';
             document.getElementById('max-download-limit').disabled = completed;
             document.getElementById('max-upload-limit').disabled = !result.bittorrent || completed;
             document.getElementById('all-proxy').disabled = completed;
-            document.getElementById('taskFiles').innerHTML = printFileInfo(result.files);
+            document.getElementById('taskProgress').innerText = completedLength + ' / ' + totalLength + ' (' + completeRatio + ')';
+            if (result.bittorrent) {
+                document.getElementById('taskFiles').style.display = 'block';
+                document.getElementById('taskFiles').innerHTML = printTaskFiles(result.files);
+                var taskName = result.bittorrent.info ? result.bittorrent.info.name : fileName;
+                var connections = result.numSeeders + ' / ' + result.connections;
+                downloadSpeed += ' / ' + uploadSpeed;
+            }
+            else {
+                document.getElementById('taskUris').style.display = 'block';
+                document.getElementById('taskUris').innerHTML = printTaskUris(result.files[0].uris);
+                taskName = fileName || url;
+                connections = result.connections;
+            }
+            document.getElementById('taskBandwidth').innerText = downloadSpeed + ' (' + connections + ')';
+            document.getElementById('taskName').innerText = taskName;
+            document.getElementById('taskName').className = 'button title ' + result.status;
         }
     );
 
-    function printFileInfo(files) {
+    function printTaskFiles(files) {
         var fileInfo = '<table>';
         files.forEach(file => {
             var filename = file.path.match(/[^\/]+$/)[0];
@@ -41,6 +54,18 @@ function printTaskDetails() {
             fileInfo += '<tr><td>' + file.index + '</td><td title="' + filePath + '">' + filename + '</td><td>' + fileSize + '</td><td>' + fileRatio + '</td></tr>';
         });
         return fileInfo + '</table>';
+    }
+
+    function printTaskUris(uris) {
+        var uriInfo = '<div><table>';
+        var uriUsed = [];
+        uris.forEach(uri => {
+            if (uri.status === 'used' && !uriUsed.includes(uri.uri)) {
+                uriUsed.push(uri.uri);
+                uriInfo += '<tr><td>' + uri.uri + '</td></tr>';
+            }
+        });
+        return uriInfo + '</table></div>';
     }
 }
 
@@ -75,9 +100,8 @@ document.getElementById('taskName').addEventListener('click', (event) => {
     parent.window.postMessage({id: 'taskMgrWindow'});
 });
 
-document.getElementById('taskFiles').addEventListener('click', (event) => {
-    if (url) {
-        navigator.clipboard.writeText(url);
-        showNotification(browser.i18n.getMessage('warn_url_copied'), url);
-    }
+document.getElementById('taskUris').addEventListener('click', (event) => {
+    var url = event.target.innerText;
+    navigator.clipboard.writeText(url);
+    showNotification(browser.i18n.getMessage('warn_url_copied'), url);
 });
