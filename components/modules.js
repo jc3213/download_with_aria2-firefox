aaea918a81922f28c9399ccdd21f83eb6830f50f
jc3213 <jc3213@users.noreply.github.com>
2021-05-21 12:47:43 +0800

    Delete modules.js

--- components/modules.js
+++
@@ -1,9 +0,0 @@
-function openModuleWindow(id, src, onload) {
-    var iframe = document.createElement('iframe');
-    iframe.id = id;
-    iframe.src = src;
-    if (typeof onload === 'function') {
-        iframe.addEventListener('load', onload);
-    }
-    document.body.appendChild(iframe);
-}
