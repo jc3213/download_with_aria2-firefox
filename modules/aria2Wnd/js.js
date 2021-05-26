09e3480b116c07aac560e63fa474da1f4fcce149
jc3213 <jc3213@users.noreply.github.com>
2021-05-26 19:03:54 +0800

    options ui now has static height not dynamic

--- modules/aria2Wnd/js.js
+++ modules/aria2Wnd/js.js
@@ -1,16 +1,9 @@
 addEventListener('message', (event) => {
     printGlobalOptions();
     document.querySelector('#aria2Ver').innerText = event.data;
-    parent.document.body.style.height = document.body.offsetHeight + 'px';
 });
 
-document.querySelector('body > div.frame > div.container').addEventListener('click', (event) => {
-    if (event.target.classList.contains('tab')) {
-        parent.document.body.style.height = document.body.offsetHeight + 'px';
-    }
-});
-
-document.querySelector('#aria2Exit').addEventListener('click', (event) => {
+document.querySelector('#back_btn').addEventListener('click', (event) => {
     parent.document.body.style.height = 'max-content';
     parent.document.querySelector('#aria2Global').remove();
 });
