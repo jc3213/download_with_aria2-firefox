50f841d4eca6f1ae4759da6ee38c76d6c8e199af
jc3213 <jc3213@users.noreply.github.com>
2021-05-29 00:02:28 +0800

    Delete card.js

--- modules/card.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('#card_btn').addEventListener('click', (event) => {
-    event.target.classList.toggle('checked');
-    document.querySelectorAll('div[card]').forEach(body => {
-        body.style.display = body.style.display === 'none' ? 'block' : 'none';
-    });
-});
