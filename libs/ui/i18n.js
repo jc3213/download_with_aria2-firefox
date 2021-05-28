2099dc5e2bdc8f559bf6b5809b62d80f3356b1e1
jc3213 <jc3213@users.noreply.github.com>
2021-05-29 00:06:18 +0800

    oops

--- libs/ui/i18n.js
+++ libs/ui/i18n.js
@@ -1,7 +1,7 @@
 document.querySelectorAll('[i18n]').forEach(item => {
-    item.innerText = chrome.i18n.getMessage(item.innerText);
+    item.innerText = browser.i18n.getMessage(item.innerText);
 });
 
 document.querySelectorAll('[i18n_title]').forEach(item => {
-    item.title = chrome.i18n.getMessage(item.title);
+    item.title = browser.i18n.getMessage(item.title);
 });
