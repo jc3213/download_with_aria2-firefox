jc3213/download_with_aria2-firefox

commit 75d130ba51632727cd778576fa9872a7fe534194

changed variables

modules/aria2Wnd/js.js
@@ -1,6 +1,6 @@
 addEventListener('message', (event) => {
     printGlobalOptions();
-    document.querySelector('#aria2Ver').innerText = event.data;
+    document.querySelector('#version').innerText = event.data;
 });
 
 document.querySelector('#back_btn').addEventListener('click', (event) => {

modules/options/js.js
@@ -29,7 +29,7 @@ document.querySelectorAll('[local]').forEach(option => {
     });
 });
 
-document.querySelector('#verify').addEventListener('click', (event) => {
+document.querySelector('#aria2_btn').addEventListener('click', (event) => {
     jsonRPCRequest(
         {method: 'aria2.getVersion'},
         (result) => {
@@ -43,7 +43,7 @@ document.querySelector('#verify').addEventListener('click', (event) => {
     );
 });
 
-document.querySelector('#insight').addEventListener('click', (event) => {
+document.querySelector('#show_btn').addEventListener('click', (event) => {
     document.querySelector('#token').setAttribute('type', event.target.classList.contains('checked') ? 'password' : 'text');
     event.target.classList.toggle('checked');
 });
@@ -52,8 +52,8 @@ document.querySelector('#capture').addEventListener('change', captureFilters);
 captureFilters();
 
 function captureFilters() {
-    document.querySelector('#captureFilters').style.display = localStorage['capture'] === '1' ? 'block' : 'none';
-    document.querySelector('#captureIgnored').style.display = localStorage['capture'] !== '0' ? 'block' : 'none';
+    document.querySelector('#filters').style.display = localStorage['capture'] === '1' ? 'block' : 'none';
+    document.querySelector('#exception').style.display = localStorage['capture'] !== '0' ? 'block' : 'none';
 }
 
 document.querySelector('#sizeEntry').addEventListener('change', calcFileSize);
@@ -65,13 +65,3 @@ function calcFileSize() {
     var unit = localStorage['sizeUnit'] | 0;
     localStorage['fileSize'] = number * 1024 ** unit;
 }
-
-document.querySelector('#output').addEventListener('change', downloadFolder);
-downloadFolder();
-
-function downloadFolder() {
-    document.querySelector('#folder').style.display = localStorage['output'] === '2' ? 'block' : 'none';
-}
-
-document.querySelector('#sizeEntry').disabled = true;
-document.querySelector('#sizeUnit').disabled = true;

modules/aria2Wnd/index.html
@@ -8,17 +8,17 @@
     <body>
         <div class="frame">
             <div class="container">
-                <span id="aria2HTTP" class="tab checked" i18n>aria2_http</span>
-                <span id="aria2BitTorrent" class="tab" i18n>aria2_bittorrent</span>
-                <span id="aria2Advanced" class="tab" i18n>common_advanced</span>
+                <span id="http" class="tab checked" i18n>aria2_http</span>
+                <span id="bt" class="tab" i18n>aria2_bittorrent</span>
+                <span id="advanced" class="tab" i18n>common_advanced</span>
             </div>
             <div class="offside">
-                <span id="aria2Ver"></span>
+                <span id="version"></span>
                 <span id="back_btn" class="button" i18n>common_back</span>
             </div>
         </div>
         <hr>
-        <div tab="aria2HTTP" class="container">
+        <div tab="http" class="container">
             <div title="aria2_http_retry_desc" i18n_title>
                 <div class="title" i18n>aria2_http_retry</div>
                 <div>
@@ -49,7 +49,7 @@
                 <span i18n>common_default</span>: <u><i>20M</i></u>
             </div>
         </div>
-        <div tab="aria2BitTorrent" class="container" style="display: none;">
+        <div tab="bt" class="container" style="display: none;">
             <div title="aria2_bt_port_desc" i18n_title>
                 <div class="title" i18n>aria2_bt_port</div>
                 <div>
@@ -89,7 +89,7 @@
                 <span i18n>common_sample</span>: <u><i>30</i></u>
             </div>
         </div>
-        <div tab="aria2Advanced" class="container" style="display: none;">
+        <div tab="advanced" class="container" style="display: none;">
             <div title="aria2_adv_dir_desc" i18n_title>
                 <div class="title" i18n>aria2_adv_dir</div>
                 <div>

modules/newTask/index.html
@@ -11,13 +11,13 @@
             <div title="task_referer_desc" i18n_title>
                 <div class="title" i18n>task_referer</div>
                 <div>
-                    <input id="taskReferer" type="text">
+                    <input id="referer" type="text">
                 </div>
             </div>
             <div title="task_batch_desc" i18n_title>
                 <div class="title" i18n>task_batch</div>
                 <div>
-                    <textarea id="taskBatch" rows="11"></textarea>
+                    <textarea id="entries" rows="11"></textarea>
                 </div>
             </div>
             <div title="task_proxy_desc" i18n_title>

modules/newTask/js.js
@@ -1,10 +1,10 @@
 document.querySelector('#submit_btn').addEventListener('click', (event) => {
-    var referer = document.querySelector('#taskReferer').value;
+    var referer = document.querySelector('#referer').value;
     var options = {};
     document.querySelectorAll('[option], [aria2]').forEach(option => {
         options[option.id] = option.value;
     });
-    document.querySelector('#taskBatch').value.split('\n').forEach(url => {
+    document.querySelector('#entries').value.split('\n').forEach(url => {
         try {
             var session = JSON.parse(url);
             if (Array.isArray(session)) {

modules/options/index.html
@@ -10,9 +10,9 @@
     <body>
         <div class="frame">
             <div class="container">
-                <span id="tabBasic" class="tab checked" i18n>option_basic</span>
-                <span id="tabAdvanced" class="tab" i18n>common_advanced</span>
-                <span id="tabDownload" class="tab" i18n>option_download</span>
+                <span id="basic" class="tab checked" i18n>option_basic</span>
+                <span id="advanced" class="tab" i18n>common_advanced</span>
+                <span id="download" class="tab" i18n>option_download</span>
             </div>
             <div id="preferences" class="offside">
                 <span id="export" class="button" i18n>option_export</span>
@@ -22,12 +22,12 @@
             </div>
         </div>
         <hr>
-        <div tab="tabBasic" class="container">
+        <div tab="basic" class="container">
             <div title="option_jsonrpc_desc" i18n_title>
                 <div class="title" i18n>option_jsonrpc</div>
                 <div>
                     <input id="jsonrpc" local type="text">
-                    <span id="verify" class="button">⚙️</span>
+                    <span id="aria2_btn" class="button">⚙️</span>
                 </div>
                 <span i18n>common_default</span>: <u><i>http://localhost:6800/jsonrpc</i></u>
             </div>
@@ -35,7 +35,7 @@
                 <div class="title" i18n>option_token</div>
                 <div>
                     <input id="token" local type="password" placeholder="$$secret$$">
-                    <span id="insight" class="button">👁️</span>
+                    <span id="show_btn" class="button">👁️</span>
                 </div>
             </div>
             <div title="option_folder_desc" i18n_title>
@@ -50,7 +50,7 @@
                 </div>
             </div>
         </div>
-        <div tab="tabAdvanced" class="container" style="display: none;">
+        <div tab="advanced" class="container" style="display: none;">
             <div title="option_useragent_desc" i18n_title>
                 <div class="title" i18n>option_useragent</div>
                 <div>
@@ -71,7 +71,7 @@
                 </div>
             </div>
         </div>
-        <div tab="tabDownload" class="container" style="display: none;">
+        <div tab="download" class="container" style="display: none;">
             <div title="option_capture_desc" i18n_title>
                 <span class="title" i18n>option_capture</span>
                 <select id="capture" local>
@@ -80,7 +80,7 @@
                     <option value="2" i18n>option_always</option>
                 </select>
             </div>
-            <div id="captureFilters" class="container">
+            <div id="filters" class="container">
                 <div title="option_filesize_desc" i18n_title>
                     <div class="title" i18n>option_filesize</div>
                     <div>
@@ -107,7 +107,7 @@
                     </div>
                 </div>
             </div>
-            <div id="captureIgnored" class="container">
+            <div id="exception" class="container">
                 <div title="option_ignored_desc" i18n_title>
                     <div class="title" i18n>option_ignored</div>
                     <div>

libs/ui/tabs.js
@@ -1,14 +1,14 @@
-document.querySelectorAll('span.tab').forEach(tab => {
+document.querySelectorAll('.tab').forEach(tab => {
     tab.addEventListener('click', (event) => {
-        document.querySelectorAll('div[tab]').forEach(body => {
-            var tabId = body.getAttribute('tab');
-            if (tabId === tab.id) {
+        document.querySelectorAll('[tab]').forEach(body => {
+            var id = body.getAttribute('tab');
+            if (id === tab.id) {
                 tab.classList.add('checked');
                 body.style.display = 'block';
             }
             else {
                 body.style.display = 'none';
-                document.getElementById(tabId).classList.remove('checked');
+                document.getElementById(id).classList.remove('checked');
             }
         });
     });

commit e89b62467fa27deaab741fa7d28d9205b039e702

optimized modules and bugfix
fixed download folder broken in 75d130b

libs/modules.css
@@ -6,17 +6,17 @@ iframe {
     left: 0px;
 }
 
-#optionsWindow, #taskMgrWindow {
+#newTask {
+    top: 40px;
+    height: calc(100% - 80px);
+}
+
+#options, #taskMgr {
     top: 0px;
     height: calc(100% - 40px);
 }
 
-#aria2Global {
+#aria2Wnd {
     top: 0px;
     height: 100%;
-}
-
-#newTaskWindow {
-    top: 40px;
-    height: calc(100% - 80px);
 }

libs/modules.js
@@ -1,9 +1,6 @@
-function openModuleWindow(id, src, onload) {
+function openModuleWindow(id, src) {
     var iframe = document.createElement('iframe');
     iframe.id = id;
     iframe.src = src;
-    if (typeof onload === 'function') {
-        iframe.addEventListener('load', onload);
-    }
     document.body.appendChild(iframe);
 }

modules/aria2Wnd/js.js
@@ -1,7 +1,4 @@
-addEventListener('message', (event) => {
-    printGlobalOptions();
-    document.querySelector('#version').innerText = event.data;
-});
+document.querySelector('#version').innerText = location.search.slice(9);
 
 document.querySelector('#back_btn').addEventListener('click', (event) => {
     parent.document.getElementById(frameElement.id).remove();
@@ -12,3 +9,5 @@ document.querySelectorAll('[aria2]').forEach(aria2 => {
         changeGlobalOption(aria2.id, aria2.value);
     });
 });
+
+printGlobalOptions();

modules/options/js.js
@@ -1,3 +1,5 @@
+document.querySelector('#manager').style.display = location.search === '?from=popup' ? 'none' : 'block';
+
 document.querySelector('#export').addEventListener('click', (event) => {
     var blob = new Blob([JSON.stringify(localStorage)], {type: 'application/json; charset=utf-8'});
     var saver = document.querySelector('#saver');
@@ -33,9 +35,7 @@ document.querySelector('#aria2_btn').addEventListener('click', (event) => {
     jsonRPCRequest(
         {method: 'aria2.getVersion'},
         (result) => {
-            openModuleWindow('aria2Global', '/modules/aria2Wnd/index.html', (event) => {
-                event.target.contentWindow.postMessage(result.version);
-            });
+            openModuleWindow('aria2Wnd', '/modules/aria2Wnd/index.html?version=' + result.version);
         },
         (error, rpc) => {
             showNotification(error, rpc);
@@ -65,3 +65,13 @@ function calcFileSize() {
     var unit = localStorage['sizeUnit'] | 0;
     localStorage['fileSize'] = number * 1024 ** unit;
 }
+
+document.querySelector('#output').addEventListener('change', downloadFolder);
+downloadFolder();
+
+function downloadFolder() {
+    document.querySelector('#folder').style.display = localStorage['output'] === '2' ? 'block' : 'none';
+}
+
+document.querySelector('#sizeEntry').disabled = true;
+document.querySelector('#sizeUnit').disabled = true;

modules/taskMgr/js.js
@@ -1,13 +1,5 @@
-var gid;
+var gid = location.search.slice(5);
 var logic = 0;
-var taskManager;
-
-addEventListener('message', (event) => {
-    gid = event.data;
-    printTaskOptions(gid);
-    printTaskManager();
-    taskManager = setInterval(printTaskManager, 1000);
-})
 
 function printTaskManager() {
     jsonRPCRequest(
@@ -37,7 +29,7 @@ function printTaskManager() {
 }
 
 function printTaskDetails(type) {
-    if (logic !== 1) {
+    if (logic === 0) {
         document.querySelectorAll('[http], [bt]').forEach(option => {
             option.style.display = option.hasAttribute(type) ? 'block' : 'none';
         });
@@ -139,3 +131,7 @@ document.querySelector('#file').addEventListener('click', (event) => {
         changeTaskOption(gid, 'select-file', checked.join());
     }
 });
+
+printTaskOptions(gid);
+printTaskManager();
+var taskManager = setInterval(printTaskManager, 1000);

popup/index.html
@@ -10,7 +10,7 @@
     <body>
         <div class="frame">
             <div id="upper" class="offside">
-                <span module="newTaskWindow" window="/modules/newTask/index.html" class="button" i18n>popup_new_button</span>
+                <span module="newTask" class="button" i18n>popup_new_button</span>
                 <span id="purdge_btn" class="button" i18n>popup_purge_button</span>
             </div>
             <div id="tabs">
@@ -63,7 +63,7 @@
         <hr>
         <div class="frame">
             <div class="offside">
-                <span module="optionsWindow" window="/modules/options/index.html" class="button" i18n>popup_options_button</span>
+                <span module="options" class="button" i18n>popup_options_button</span>
             </div>
             <div id="bandwidth">
                 <span class="active" i18n>popup_download</span>

popup/js.js
@@ -1,11 +1,7 @@
 document.querySelectorAll('[module]').forEach(module => {
     var id = module.getAttribute('module');
-    var src = module.getAttribute('window');
-    if (id === 'optionsWindow') {
-        var onload = (event) => {
-            event.target.contentDocument.querySelector('#preferences').style.display = 'none';
-        };
-    }
+    var src = '/modules/' + id + '/index.html?from=popup';
+
     module.addEventListener('click', (event) => {
         if (event.target.classList.contains('checked')) {
             document.getElementById(id).remove();
@@ -143,9 +139,7 @@ function removeTaskFromQueue(gid, status) {
 }
 
 function openTaskMgrWindow(gid) {
-    openModuleWindow('taskMgrWindow', '/modules/taskMgr/index.html', (event) => {
-        event.target.contentWindow.postMessage(gid);
-    });
+    openModuleWindow('taskMgr', '/modules/taskMgr/index.html?gid=' + gid);
 }
 
 function removeTaskAndRetry(gid) {