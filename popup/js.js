jc3213/download_with_aria2-firefox

commit 9fb56dce030dba6ab3651834b228c3b4f04d9cf5

optimized tabs, task filters and other

popup/js.js
@@ -13,19 +13,20 @@ document.querySelectorAll('[module]').forEach(module => {
     });
 });
 
-document.querySelectorAll('.tab').forEach(tab => {
+document.querySelectorAll('[tab]').forEach(tab => {
+    var active = tab.getAttribute('tab');
     tab.addEventListener('click', (event) => {
-        document.querySelectorAll('[tab]').forEach(body => {
-            var tabId = body.getAttribute('tab');
+        document.querySelectorAll('[panel]').forEach(panel => {
+            var id = panel.getAttribute('panel');
             if (tab.classList.contains('checked')) {
-                body.style.display = 'block';
+                panel.style.display = 'block';
             }
-            else if (tabId === tab.id) {
-                body.style.display = 'block';
+            else if (id === active) {
+                panel.style.display = 'block';
             }
             else {
-                body.style.display = 'none';
-                document.getElementById(tabId).classList.remove('checked');
+                panel.style.display = 'none';
+                document.querySelector('[tab="' + id + '"]').classList.remove('checked');
             }
         });
         tab.classList.toggle('checked');
@@ -49,7 +50,7 @@ function printTaskManager() {
         {method: 'aria2.tellActive'},
         {method: 'aria2.tellWaiting', index: [0, 9999]},
         {method: 'aria2.tellStopped', index: [0, 9999]}
-    ], (global, active, waiting, stopped) => {
+    ], (global, running, suspend, aborted) => {
         document.querySelector('#active').innerText = global.numActive;
         document.querySelector('#waiting').innerText = global.numWaiting;
         document.querySelector('#stopped').innerText = global.numStopped;
@@ -58,9 +59,9 @@ function printTaskManager() {
         document.querySelector('#tabs').style.display = 'block';
         document.querySelector('#upper').style.display = 'block';
         document.querySelector('#network').style.display = 'none';
-        active.forEach(printTaskDetails);
-        waiting.forEach(printTaskDetails);
-        stopped.forEach(printTaskDetails);
+        running.forEach((running, index) => printTaskDetails(running, index, document.querySelector('[panel="running"]')));
+        suspend.forEach((suspend, index) => printTaskDetails(suspend, index, document.querySelector('[panel="suspend"]')));
+        aborted.forEach((aborted, index) => printTaskDetails(aborted, index, document.querySelector('[panel="aborted"]')));
     }, (error, rpc) => {
         document.querySelector('#tabs').style.display = 'none';
         document.querySelector('#upper').style.display = 'none';
@@ -69,10 +70,9 @@ function printTaskManager() {
     });
 }
 
-function printTaskDetails(result, index) {
+function printTaskDetails(result, index, queue) {
     var task = document.getElementById(result.gid) || appendTaskDetails(result);
     if (task.status !== result.status) {
-        var queue = document.querySelector('[queue="' + result.status + '"]');
         queue.insertBefore(task, queue.childNodes[index]);
         task.status = result.status;
     }

modules/taskMgr/js.js
@@ -15,7 +15,7 @@ function printTaskManager() {
                 result.files[0].uris.forEach(uri => printTaskUris(uri, document.querySelector('#uri')));
             }
             document.querySelector('#name').innerText = result.bittorrent && result.bittorrent.info ? result.bittorrent.info.name : result.files[0].path.slice(result.files[0].path.lastIndexOf('/') + 1) || result.files[0].uris[0].uri;
-            document.querySelector('#name').className = result.status + ' button';
+            document.querySelector('#name').className = result.status;
             document.querySelector('#local').innerText = bytesToFileSize(result.completedLength);
             document.querySelector('#ratio').innerText = ((result.completedLength / result.totalLength * 10000 | 0) / 100) + '%';
             document.querySelector('#remote').innerText = bytesToFileSize(result.totalLength);

components/common.css
@@ -11,13 +11,6 @@ body {
     word-break: break-word;
 }
 
-.tab, .button {
-    border: 1px outset #ccc;
-    border-radius: 3px;
-    cursor: pointer;
-    padding: 3px 5px;
-}
-
 .frame {
     height: 20px;
 }
@@ -30,11 +23,18 @@ body {
     text-align: right;
 }
 
-.button:hover, .tab:hover {
+[tab], [button] {
+    border: 1px outset #ccc;
+    border-radius: 3px;
+    cursor: pointer;
+    padding: 3px 5px;
+}
+
+[tab]:hover, [button]:hover {
     filter: opacity(60%);
 }
 
-.button:active, .tab:active {
+[tab]:active, [button]:active {
     filter: opacity(30%);
 }
 

libs/ui/common.css
@@ -1,8 +1,8 @@
-[tab], [card] {
+[panel], [card] {
     overflow-y: auto;
 }
 
-[tab] {
+[panel] {
     height: 400px;
 }
 

libs/ui/tabs.js
@@ -1,15 +1,23 @@
-document.querySelectorAll('.tab').forEach(tab => {
+document.querySelectorAll('[tab]').forEach((tab, index) => {
+    var active = tab.getAttribute('tab');
+    tab.className = index === 0 ? 'checked' : '';
     tab.addEventListener('click', (event) => {
-        document.querySelectorAll('[tab]').forEach(body => {
-            var id = body.getAttribute('tab');
-            if (id === tab.id) {
-                tab.classList.add('checked');
-                body.style.display = 'block';
-            }
-            else {
-                body.style.display = 'none';
-                document.getElementById(id).classList.remove('checked');
-            }
-        });
+        if (!tab.classList.contains('checkd')) {
+            document.querySelectorAll('[panel]').forEach(panel => {
+                var id = panel.getAttribute('panel');
+                if (id === active) {
+                    tab.className = 'checked';
+                    panel.style.display = 'block';
+                }
+                else {
+                    panel.style.display = 'none';
+                    document.querySelector('[tab="' + id + '"]').removeAttribute('class');
+                }
+            });
+        }
     });
+});
+
+document.querySelectorAll('[panel]').forEach((panel, index) => {
+    panel.style.display = index === 0 ? 'block' : 'none';
 });

modules/aria2Wnd/index.html
@@ -8,17 +8,17 @@
     <body>
         <div class="frame">
             <div class="container">
-                <span id="http" class="tab checked" i18n>aria2_http</span>
-                <span id="bt" class="tab" i18n>aria2_bittorrent</span>
-                <span id="advanced" class="tab" i18n>common_advanced</span>
+                <span tab="http" class="checked" i18n>aria2_http</span>
+                <span tab="bt" i18n>aria2_bittorrent</span>
+                <span tab="advanced" i18n>common_advanced</span>
             </div>
             <div class="offside">
                 <span id="version"></span>
-                <span id="back_btn" class="button" i18n>common_back</span>
+                <span id="back_btn" button i18n>common_back</span>
             </div>
         </div>
         <hr>
-        <div tab="http" class="container">
+        <div panel="http" class="container">
             <div title="aria2_http_retry_desc" i18n_title>
                 <div class="title" i18n>aria2_http_retry</div>
                 <div>
@@ -49,7 +49,7 @@
                 <span i18n>common_default</span>: <u><i>20M</i></u>
             </div>
         </div>
-        <div tab="bt" class="container" style="display: none;">
+        <div panel="bt" class="container">
             <div title="aria2_bt_port_desc" i18n_title>
                 <div class="title" i18n>aria2_bt_port</div>
                 <div>
@@ -89,7 +89,7 @@
                 <span i18n>common_sample</span>: <u><i>30</i></u>
             </div>
         </div>
-        <div tab="advanced" class="container" style="display: none;">
+        <div panel="advanced" class="container">
             <div title="aria2_adv_dir_desc" i18n_title>
                 <div class="title" i18n>aria2_adv_dir</div>
                 <div>

modules/newTask/index.html
@@ -24,7 +24,7 @@
                 <div class="title" i18n>task_proxy</div>
                 <div>
                     <input id="all-proxy" option type="text">
-                    <span id='allproxy' load="all-proxy" class="button">📷</span>
+                    <span id='allproxy' load="all-proxy" button>📷</span>
                 </div>
             </div>
         </div>
@@ -62,7 +62,7 @@
         </div>
         <hr>
         <div class="submenu">
-            <span id="submit_btn" class="button" i18n>task_submit</span>
+            <span id="submit_btn" button i18n>task_submit</span>
         </div>
         <script src="/components/jsonrpc.js"></script>
         <script src="/libs/ui/i18n.js"></script>

modules/options/index.html
@@ -10,24 +10,24 @@
     <body>
         <div class="frame">
             <div class="container">
-                <span id="basic" class="tab checked" i18n>option_basic</span>
-                <span id="advanced" class="tab" i18n>common_advanced</span>
-                <span id="download" class="tab" i18n>option_download</span>
+                <span tab="basic" class="checked" i18n>option_basic</span>
+                <span tab="advanced" i18n>common_advanced</span>
+                <span tab="download" i18n>option_download</span>
             </div>
             <div id="manager" class="offside">
-                <span id="export" class="button" i18n>option_export</span>
+                <span id="export" button i18n>option_export</span>
                 <a id="saver" style="display: none;"></a>
-                <span id="import" class="button" i18n>option_import</span>
+                <span id="import" button i18n>option_import</span>
                 <input id="reader" type="file" accept="application/json" style="display: none;">
             </div>
         </div>
         <hr>
-        <div tab="basic" class="container">
+        <div panel="basic" class="container">
             <div title="option_jsonrpc_desc" i18n_title>
                 <div class="title" i18n>option_jsonrpc</div>
                 <div>
                     <input id="jsonrpc" local type="text">
-                    <span id="aria2_btn" class="button">⚙️</span>
+                    <span id="aria2_btn" button>⚙️</span>
                 </div>
                 <span i18n>common_default</span>: <u><i>http://localhost:6800/jsonrpc</i></u>
             </div>
@@ -35,22 +35,11 @@
                 <div class="title" i18n>option_token</div>
                 <div>
                     <input id="token" local type="password" placeholder="$$secret$$">
-                    <span id="show_btn" class="button">👁️</span>
-                </div>
-            </div>
-            <div title="option_folder_desc" i18n_title>
-                <span class="title" i18n>option_folder</span>
-                <select id="output" local>
-                    <option value="0" i18n>common_default</option>
-                    <option value="1" i18n>option_folder_browser</option>
-                    <option value="2" i18n>option_folder_custom</option>
-                </select>
-                <div>
-                    <input id="folder" local gear="output&2" type="text" placeholder="C:\Aria2 Downloads">
+                    <span id="show_btn" button>👁️</span>
                 </div>
             </div>
         </div>
-        <div tab="advanced" class="container" style="display: none;">
+        <div panel="advanced" class="container">
             <div title="option_useragent_desc" i18n_title>
                 <div class="title" i18n>option_useragent</div>
                 <div>
@@ -71,7 +60,7 @@
                 </div>
             </div>
         </div>
-        <div tab="download" class="container" style="display: none;">
+        <div panel="download" class="container">
             <div title="option_capture_desc" i18n_title>
                 <span class="title" i18n>option_capture</span>
                 <select id="capture" local>

commit 1daa696754355dec0e4edfbae4200704ff5b8841

fixed broken purge button in 9fb56dc

popup/js.js
@@ -37,9 +37,7 @@ document.querySelector('#purdge_btn').addEventListener('click', (event) => {
     jsonRPCRequest(
         {method: 'aria2.purgeDownloadResult'},
         (result) => {
-            document.querySelector('[queue="complete"]').innerHTML = '';
-            document.querySelector('[queue="error"]').innerHTML = '';
-            document.querySelector('[queue="removed"]').innerHTML = '';
+            document.querySelector('[panel="aborted"]').innerHTML = '';
         }
     );
 });