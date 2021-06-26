532fab48622be925cb874caef00865cdd658dd38
jc3213 <jc3213@users.noreply.github.com>
2021-06-26 22:56:58 +0800

    minor optimization

--- modules/options/js.js
+++ modules/options/js.js
@@ -54,10 +54,7 @@ document.querySelector('#import').addEventListener('click', (event) => {
 
 document.querySelector('#aria2_btn').addEventListener('click', (event) => {
     browser.runtime.sendMessage({jsonrpc: true}, aria2RPC => {
-        var {version, error} = aria2RPC;
-        if (version) {
-            openModuleWindow('aria2Wnd', '/modules/aria2Wnd/index.html?' + version.version);
-        }
+        openModuleWindow('aria2Wnd', '/modules/aria2Wnd/index.html?' + aria2RPC.version.version);
     });
 });
 
